f7b8d92dacdd020d2db73ae7650a3e938dae20b3
Kavin Karthick R <144412212+Kavinkarthick2005@users.noreply.github.com>
2025-02-11 15:20:50 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value === "") return;
+
+    let li = document.createElement("li");
+    li.innerHTML = `${taskInput.value} <button onclick="removeTask(this)">X</button>`;
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
+
+function removeTask(button) {
+    button.parentElement.remove();
+}
